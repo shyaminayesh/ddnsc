403c6a89f2dc186a66feee7fb86328320c8b4689
Shyamin Ayesh <me@shyamin.com>
2022-01-28 04:37:13 +0530

    Initial POC

--- ddnsc.go
+++ ddnsc.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"ddnsc/factories"
+	"net/http"
+	"reflect"
+	"time"
+)
 
 func main() {
-	fmt.Println("test")
+
+	/**
+	* We have to load the configuration file from the disk to
+	* grab predefine configuration data to the utility.
+	 */
+	config := Config()
+
+	// ip := &net.IP{}
+	for {
+
+		for name, conf := range config.GetStringMap("provider") {
+			Provider := factories.Providers[name]
+
+			/**
+			* Configure struct attribute for future use in the
+			* provider methods. We can use common set of interfaces
+			* if needed.
+			 */
+			reflect.ValueOf(Provider).Elem().FieldByName("Client").Set(reflect.ValueOf(&http.Client{}))
+
+			/**
+			* Invoke the worker method of the provider plugin
+			* to do the actual work.
+			 */
+			reflect.ValueOf(Provider).MethodByName("Worker").Call([]reflect.Value{
+				reflect.ValueOf(conf),
+			})
+
+		}
+
+		time.Sleep(time.Duration(config.GetInt("global.interval")) * time.Second)
+	}
+
 }
